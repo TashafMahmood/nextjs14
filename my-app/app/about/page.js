e80eab1280c97e0c4914c58cac9055fac6721042
TashafMahmood <tashaf@elred.io>
2024-06-29 13:45:44 +0530

    generate metadata

---
+++ my-app/app/about/page.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About = () => {
+  return <div>About page</div>;
+};
+
+export default About;
+
+export const generateMetadata = () => {
+  return {
+    title: "About page",
+    description: "This is all about page",
+  };
+};
