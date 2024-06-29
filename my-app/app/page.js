6e961fa9f0b78cf474eb6dbe1d31470fc1a44f88
TashafMahmood <tashaf@elred.io>
2024-06-29 10:43:33 +0530

    create useRouter navigation

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,11 +1,31 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { useRouter } from "next/navigation";
 
 const page = () => {
+  const router = useRouter();
+
+  const navigate = (page) => {
+    router.push(page);
+  };
+
   return (
     <div>
-      Home
+      <h1 className="text-3xl">Home</h1>
+      <button
+        className="bg-slate-400 p-2 rounded-md shadow-lg my-2 hover:bg-slate-500 cursor-pointer transition-all ease-in"
+        onClick={() => router.push("about")}
+      >
+        Go to About
+      </button>
+      <button
+        className="bg-red-500 p-2 rounded-md shadow-lg my-2 hover:bg-red-600 cursor-pointer transition-all ease-in text-white ml-2"
+        onClick={() => navigate("login")}
+      >
+        Login Now
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
