882c9564dd93f3520df1aede53b8143158e38d4a
TashafMahmood <tashaf@elred.io>
2024-06-29 11:17:30 +0530

    created dynamic route

---
+++ my-app/app/userlist/[user]/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const User = ({ params }) => {
+  return <div>Info About {params.user}</div>;
+};
+
+export default User;
--- my-app/app/userlist/page.js
+++ my-app/app/userlist/page.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Login = () => {
+const UserList = () => {
   return (
     <div>
-      Login
+      UserList
     </div>
   )
 }
 
-export default Login
+export default UserList
