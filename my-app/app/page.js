04dfea09ee3d363b33568686f273051c1394fbce
TashafMahmood <tashaf@elred.io>
2024-06-29 10:50:10 +0530

    ignoring routes

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -12,18 +12,6 @@ const page = () => {
   return (
     <div>
       <h1 className="text-3xl">Home</h1>
-      <button
-        className="bg-slate-400 p-2 rounded-md shadow-lg my-2 hover:bg-slate-500 cursor-pointer transition-all ease-in"
-        onClick={() => router.push("about")}
-      >
-        Go to About
-      </button>
-      <button
-        className="bg-red-500 p-2 rounded-md shadow-lg my-2 hover:bg-red-600 cursor-pointer transition-all ease-in text-white ml-2"
-        onClick={() => navigate("login")}
-      >
-        Login Now
-      </button>
     </div>
   );
 };
