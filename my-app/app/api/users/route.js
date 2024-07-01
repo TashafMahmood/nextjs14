60b49986bb2997e056b7b615fdbc9fe67d377b30
TashafMahmood <tashaf@elred.io>
2024-07-01 16:37:56 +0530

    added .env files

--- my-app/app/api/users/route.js
+++
@@ -1,6 +0,0 @@
-import { users } from "@/app/utils/db";
-import { NextResponse } from "next/server";
-
-export function GET() {
-  return NextResponse.json({ data: users });
-}
---
+++ my-app/app/random/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const RandomPage = () => {
+    console.log(process.env.NODE_ENV)
+  return (
+    <div>
+      This is random page..
+    </div>
+  )
+}
+
+export default RandomPage
