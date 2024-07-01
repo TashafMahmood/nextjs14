057df75d2db7f146fb971b99bef41288d0c8aa44
TashafMahmood <tashaf@elred.io>
2024-07-01 12:15:01 +0530

    nextjs fonts created

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,21 +1,13 @@
 "use client";
 import React from "react";
-import { useRouter } from "next/navigation";
-import Image from 'next/image'
-import natureImage from '@/public/test.jpg'
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({ subsets: ["latin"], weight: "500" });
 
 const page = () => {
-  const router = useRouter();
-
-  const navigate = (page) => {
-    router.push(page);
-  };
-
   return (
     <div>
-      <h1 className="text-3xl">Home</h1>
-      {/* <Image src={natureImage} alt="nature image" width={500} height={500}/> */}
-      <Image src="https://images.unsplash.com/photo-1458693416646-07f13b1a3159?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="nature image" width={500} height={500}/>
+      <div>lorem ipsum font family is Roboto.</div>
     </div>
   );
 };
